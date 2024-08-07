681ad6c832c0f776106c35771f531927f9e1aa6c
Mattia Alfonso <alfonsomattia@gmail.com>
2024-08-07 08:42:08 +0200

    Added solution for CodeSize

--- CodeSize/src/CodeSize.sol
+++ CodeSize/src/CodeSize.sol
@@ -5,4 +5,21 @@ contract CodeSize {
     /**
      * The challenge is to create a contract whose runtime code (bytecode) size is greater than 1kb but less than 4kb
      */
+     
+    // Add random fields here to increase the code size
+    struct Person {
+        string firstName;
+        string lastName;
+        string nickname;
+    }
+    Person[] public array;
+
+    function push(Person calldata newElement) public {
+        array.push(newElement);
+    }
+
+    function pop() public returns (Person memory lastElement) {
+        lastElement = array[array.length - 1];
+        array.pop();
+    }
 }
