55e2a8bd9494e610e0f78a0cc367bfdff70f8715
Mattia Alfonso <alfonsomattia@gmail.com>
2024-08-07 08:44:43 +0200

    Added solution for DeployContract

--- DeployContract/src/DeployContract.sol
+++ DeployContract/src/DeployContract.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
+contract Deployed {
+    function test() public pure returns (string memory) {
+        return "Hello world!";
+    }
+}
+
 contract Deployer {
     /*
         This exercise assumes you know how to deploy a contract.
@@ -8,7 +14,13 @@ contract Deployer {
         2. Deploy the contract and return the address in `deployContract` function.
     */
 
+    Deployed deployed;
+
+    constructor() {
+        deployed = new Deployed();
+    }
+
     function deployContract() public view returns (address) {
-        // your code here
+        return address(deployed);
     }
 }
